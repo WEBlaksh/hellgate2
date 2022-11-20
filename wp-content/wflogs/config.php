36c48583d9c1235446b8e4dffa373cbf9829aca0
WEBlaksh <114424458+WEBlaksh@users.noreply.github.com>
2022-11-20 19:49:18 +0530

    Delete wp-content directory

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1666713935;s:7:"authKey";s:64:"uJDQzSi^k3ILZFC[(D2xE GkEe*$:%?4H4Pt/~J*Ol39-n$nX?$zYqZDn1v7n]Bx";s:7:"version";s:5:"1.0.5";}
